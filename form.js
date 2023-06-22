9fd1583aa102f1d1a23c81096c2564d0a607ec6a
thomaschor <99774559+thomaschor@users.noreply.github.com>
2023-06-21 17:27:01 -0700

    Begin email form validation again

--- form.js
+++ form.js
@@ -10,5 +10,31 @@
 // }
 // verifyPassword()
 
+const form = document.querySelector("form");
 const email = document.getElementById("mail");
+const emailError = document.querySelector("#mail + span.error");
+
+email.addEventListener("input", (event) => { 
+    if(email.validity.valid) { 
+        emailError.textContent = ""; 
+        emailError.className = "error"; 
+    } else { 
+        showError();
+    }
+});
+
+form.addEventListener("submit", (event) => { 
+    if (!email.validity.valid) { 
+        showError();
+        event.preventDefault(); 
+    }
+});
+
+function showError() { 
+    if (email.validity.valueMissing) { 
+        emailError.textContent = "You need ot enter an email address.";
+    } else if (email.validity.typeMismatch) { 
+        emailError.textContent = "Entered value needs to be an email address."
+    }
+}
 
