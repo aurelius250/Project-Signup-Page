2eebbbe12c756153e4cd05b509aa2895bd53dc47
thomaschor <99774559+thomaschor@users.noreply.github.com>
2023-06-23 14:36:15 -0700

    Further validation work

--- form.js
+++ form.js
@@ -10,49 +10,116 @@
 // }
 // verifyPassword()
 
+
 const form = document.querySelector("form");
 const email = document.getElementById("mail");
 const emailError = document.querySelector("#mail + span.error");
 const firstNameError = document.querySelector("#first-name + span.error");
 const firstName = document.getElementById("first-name");
+const pwd = document.getElementById("pwd");
+const confirmPwd = document.getElementById("confirm-pwd");
+const pwdError = document.querySelector("#pwd + span.error");
+const confirmPwdError = document.querySelector("#confirm-pwd + span.error"); 
 
 email.addEventListener("input", (event) => { 
-    if(email.validity.valid) { 
+    
+    if (email.value.length === 0) { 
+        emailError.textContent = ""; 
+        emailError.className = "error"; 
+    } 
+    else if(email.validity.valid) { 
         emailError.textContent = ""; 
         emailError.className = "error"; 
     } else { 
         showError();
     }
+
 });
 
-firstName.addEventListener("input", (event) => { 
-    if (firstName.value.length === 0) { 
-        firstNameError.textContent = ""; 
-        firstNameError.className = "error"; 
-    } else { 
-        namesShowError();
-    } 
+confirmPwd.addEventListener("input", (event) => { 
+    if (confirmPwd.value.length === 0) { 
+        confirmPwdError.textContent = ""; 
+        confirmPwdError.className = "error";
+    } else if (isValidPwd()) { 
+        confirmPwdError.textContent = ""; 
+        confirmPwdError.className = "error";
+        pwdError.textContent = ""; 
+        pwdError.className = "error"
+    }
+})
+
+pwd.addEventListener("input", (event) => { 
+    if (pwd.value.length === 0) { 
+        pwdError.textContent = ""; 
+        pwdError.className = "error"
+    } else if (isValidPwd()){ 
+        pwdError.textContent = ""; 
+        pwdError.className = "error"
+        confirmPwdError.textContent = ""; 
+        confirmPwdError.className = "error";
+    }
 });
 
 form.addEventListener("submit", (event) => { 
-    if (!email.validity.valid) { 
+
+    if (pwd.value.length === 0) { 
+        pwdError.textContent = "Please enter a password"; 
+        pwdError.className = "error active"; 
+        event.preventDefault(); 
+    } else if (pwd.validity.tooShort) { 
+            pwdError.textContent = `Password should be at least ${pwd.minLength} characters; you entered ${pwd.value.length}.`; 
+            event.preventDefault(); 
+
+
+    } else if (confirmPwd.value.length === 0) { 
+        confirmPwdError.textContent = "Please enter a password"; 
+        confirmPwdError.className = "error active"; 
+        event.preventDefault(); 
+
+    } else if (!isValidPwd()) { 
+        pwdError.textContent = "Please enter matching passwords.";
+        pwdError.className = "error active";
+        confirmPwdError.textContent = "Please enter matching passwords.";
+        confirmPwdError.className = "error active";
+        event.preventDefault(); 
+    }
+    else if (!email.validity.valid) { 
         showError();
         event.preventDefault(); 
     } 
 
-    if (!firstName.validity.valid) { 
+    else if (!firstName.validity.valid) { 
         namesShowError();
         event.preventDefault();
     }
 });
 
+firstName.addEventListener("input", (event) => { 
+    var regexForName = /^[a-z ,.'-]+$/i;
+
+    if (firstName.value.length === 0) { 
+        firstNameError.textContent = ""; 
+        firstNameError.className = "error"; 
+    } 
+    else if (regexForName.test(firstName.value)) { 
+        firstNameError.textContent = ""; 
+        firstNameError.className = "error"; 
+    } 
+    else { 
+        namesShowError();
+    }
+    } 
+);
+
+
 function showError() { 
     if (email.validity.valueMissing) { 
         emailError.textContent = "You need to enter an email address.";
-    } else if (email.validity.typeMismatch) { 
+    } 
+    else if (email.validity.typeMismatch) { 
         emailError.textContent = "Entered value needs to be an email address."
     } else if (email.validity.tooShort) { 
-        emailError.textContent = "Email should be at least ${email.minLength} characters; you entered ${email.value.length}."; 
+        emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`; 
     }
 
     emailError.className = "error active"; 
@@ -69,6 +136,12 @@ function namesShowError() {
 }
 
 
-
+function isValidPwd() { 
+    if (pwd.value != confirmPwd.value) { 
+        return false; 
+    } else {
+        return true;
+    }
+}
 
 
